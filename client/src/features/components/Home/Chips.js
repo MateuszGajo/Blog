304cd3a5154272924e680e5b537e34f0299fe3e3
Mateusz Gajo <mateusz.gajo9@wp.pl>
2019-07-27 11:10:47 +0200

    improved code

--- client/src/features/components/Home/Chips.js
+++ client/src/features/components/Home/Chips.js
@@ -57,7 +57,7 @@ const Chips = props => {
       setCategories(props.category.categories);
     }
     setPosts(props.posts);
-  }, [props.posts, props.category]);
+  }, [props.posts]);
   return (
     <React.Fragment>
       <Grid item xs={12}>
